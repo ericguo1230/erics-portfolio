36ace3c7c21cb28202988658608fadad5a562caf
ERIC <eric.guo1230@gmail.com>
2025-07-08 23:52:19 -0400

    start

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,30 @@
+export default function Navbar() {
+
+    return (
+        <div className="navbar bg-base-100 shadow-lg">
+            <div className="flex-none">
+                <button className="btn btn-square btn-ghost">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path> </svg>
+                </button>
+            </div>
+            <div className="navbar-start">
+                <a href='/'className="btn btn-ghost text-xl">Eric's Portfolio</a>
+            </div>
+            <div className="navbar-center hidden lg:flex gap-8">
+                <a href='/about' className="btn btn-ghost text-l">About</a>
+                <a href='/projects' className="btn btn-ghost text-l">Projects</a>
+                <a href='/contact' className="btn btn-ghost text-l">Contact</a>
+            </div>
+            <div className="navbar-end">
+                <label className="toggle text-base-content">
+                <input type="checkbox" value="synthwave" className="theme-controller" />
+
+                <svg aria-label="sun" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><circle cx="12" cy="12" r="4"></circle><path d="M12 2v2"></path><path d="M12 20v2"></path><path d="m4.93 4.93 1.41 1.41"></path><path d="m17.66 17.66 1.41 1.41"></path><path d="M2 12h2"></path><path d="M20 12h2"></path><path d="m6.34 17.66-1.41 1.41"></path><path d="m19.07 4.93-1.41 1.41"></path></g></svg>
+
+                <svg aria-label="moon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path></g></svg>
+
+                </label>
+            </div>
+        </div>
+    )
+}
