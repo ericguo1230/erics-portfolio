0c0c452aa77f913ae908f00c31de4a959356ee2e
ERIC <eric.guo1230@gmail.com>
2025-12-02 10:45:56 -0500

    fix dock home route

--- src/app/(browser-layout)/components/dock.tsx
+++ src/app/(browser-layout)/components/dock.tsx
@@ -12,7 +12,7 @@ export default function Dock(){
             bottom: 'auto', 
             left: 'auto'
         }}>
-            <Link className={`${path==='/' ? 'dock-active' : ''}`} href='/'>
+            <Link className={`${path==='/' ? 'dock-active' : ''}`} href='.'>
                 <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g fill="currentColor" strokeLinejoin="miter" strokeLinecap="butt"><polyline points="1 11 12 2 23 11" fill="none" stroke="currentColor" strokeMiterlimit="10" strokeWidth="2"></polyline><path d="m5,13v7c0,1.105.895,2,2,2h10c1.105,0,2-.895,2-2v-7" fill="none" stroke="currentColor" strokeLinecap="square" strokeMiterlimit="10" strokeWidth="2"></path><line x1="12" y1="22" x2="12" y2="18" fill="none" stroke="currentColor" strokeLinecap="square" strokeMiterlimit="10" strokeWidth="2"></line></g></svg>
                 <span className="dock-label">Home</span>
             </Link>
